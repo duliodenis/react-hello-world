fd5fa003187c1680924b8a2f7d0505f61c22df1d
Dulio Denis <dulio.denis@yahoo.com>
2018-02-23 21:37:39 -0500

    Using dynamic data

--- helloworld/src/components/header.js
+++ helloworld/src/components/header.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+const getYear = () => {
+    const newDate = new Date();
+    return newDate.getFullYear();
+}
+
 const Header = () => {
-    return <div>This is the new Header</div>
+    return <div>The year is {
+        getYear()
+    }</div>
 } 
 
 export default Header;
