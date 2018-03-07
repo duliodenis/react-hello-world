a01efd5fc56e8ad23790560675a8ff722328896e
Dulio Denis <dulio.denis@yahoo.com>
2018-03-06 20:30:24 -0500

    Added header component styles via css stylesheet

--- helloworld/src/components/header.js
+++ helloworld/src/components/header.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import '../css/styles.css'
 
 class Header extends Component {
     render() {
+        // const styles = {
+        //     header: {
+        //         background: '#03a9f4'
+        //     },
+        //     logo: {
+        //         color: '#fff',
+        //         fontFamily: 'Anton',
+        //         textAlign: 'center'
+        //     }
+        // }
+
         return (
-            <header>
-                <div> Logo </div>
+            <header> 
+                <div className="logo"> Logo </div>
                 <input type="text"/>
             </header>
         )
